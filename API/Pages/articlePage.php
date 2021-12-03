8218c049668c6ff8eb9c7c8392d5f4b3974c4fd0
Lopuf <maxcalio28@gmail.com>
2021-12-03 07:15:20 +0100

    éfsrdhjy

---
+++ API/Pages/articlePage.php
@@ -0,0 +1,23 @@
+<head> 
+    <meta charset="utf-8"/>
+    <link rel="icon" href="../../Front/images/favicon.png" />
+    <meta name="viewport" content="width=devise-width, initial-scale=0.5">
+    <title> 
+            Le Sauvetage Minimal
+    </title>
+    <link rel="stylesheet" href="../../Front/style.css"/>
+    <script src="../JS/ajax.js"></script>
+    <script src="../JS/getArticleCreation.js"></script>
+</head>
+<body>
+    <div class="content">
+        <div id="title">
+            <h2>test</h2>
+        </div>
+        <div id="date">10.22.20</div>
+        <div id="description">aze zzefjzf fzeu fzbf zfhzeb fhbuhfb zejf zjbjh bzjf hzbbf jzhbfh z fh z bfhzbvfj z bfjhzb fjzh bjez fjsh g hj bj gb fdjbg brdkj brkd ghjsd jhrdhb gjdf gjdf gjer gnd jg brej gjdbjkgbxjdbgfd; gdkjercfdjb cvjk b,c  bdjhcvgnj
+         </div>
+        <div id="image"><img src="../../Front/images/articleIcon.png"></div>
+
+    </div>
+</body>
