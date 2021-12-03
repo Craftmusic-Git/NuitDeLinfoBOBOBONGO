03085d9b3adca6ab9b60bfcb711a75a969b9d48c
Michel Stegmiller <michel.stegmiller@uha.fr>
2021-12-03 03:51:24 +0100

    Modification des fichiers de process

--- API/procedure/bdd.php
+++ API/procedure/bdd.php
@@ -1,8 +1,10 @@
 <?php
-    function connect(){
-        $rep = mysqli_connect("cocobongo.hopto.org.3306","cocobongo","cacabongo");
-
-        return $rep;
+    function connect(){ //3306
+        $bdd = mysqli_connect("cocobongo.hopto.org","cocobongo","cacabongo","cocobongo","3306");
+        if(!$bdd){
+            echo"ERROR";
+        }
+        return $bdd;
     }
 
     function SQLclose($dataBase){
--- API/procedure/processPerson.php
+++ API/procedure/processPerson.php
@@ -1,1 +1,15 @@
+
 <?php
+    include_once "bdd.php";
+
+    function FromNameToPerson($Name){
+        $bdd = connect();
+        if($bdd != null){
+            if( !$res = $bdd->query("CALL FromNameToPerson('$Name')")) {
+                printf("Error with the call of : FromNameToPerson");
+                return $res;
+            }
+        }
+        return null;
+    }
+?>
