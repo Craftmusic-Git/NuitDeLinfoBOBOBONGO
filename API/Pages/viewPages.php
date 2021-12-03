6a124299105b55cd7e9b6d291ce8b811eca37db6
Lopuf <maxcalio28@gmail.com>
2021-12-03 05:04:49 +0100

    template view

---
+++ API/Pages/viewPages.php
@@ -0,0 +1,26 @@
+<?php
+
+sendContentToDom();
+
+function sendContentToDom(){
+    $contentDOM = '<div id="template-page">';
+    $contentDOM .= getArticleData(); // boucle sur tous les articles
+    $contentDOM .= '</div>';
+
+    echo $contentDOM;
+}
+
+function getArticleData(){
+    return '
+      <div class="div-article">
+        <div class="article-icon">
+        </div>
+        <div class="article-data">
+            <h3>
+            </h3>
+            <p>
+            </p>
+        </div>
+      </div>  
+    ';
+}
