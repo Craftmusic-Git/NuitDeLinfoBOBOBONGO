acee8cf358618a0b6172a0ea3f34394eb3571c1f
thib_info <thibault.b45@gmail.com>
2021-12-03 03:01:48 +0100

    restore adminPage

---
+++ API/JS/getArticleCreation.js
@@ -0,0 +1,9 @@
+
+function getInputs(){
+    var titleArticle = document.getElementsByClassName('titleArticle')[0].textContent;
+    var personneAssociate = document.getElementsByClassName('personAssociate')[0].textContent;
+    var eventAssociate = document.getElementsByClassName('eventAssociate')[0].textContent;
+    var boatAssociate = document.getElementsByClassName('boatAssociate')[0].textContent;
+    var contentArticle = document.getElementsByClassName('article')[0].textContent;
+    
+}
---
+++ API/Pages/adminPage.php
@@ -0,0 +1,49 @@
+<head> 
+    <meta charset="utf-8"/>
+    <link rel="icon" href="../../Front/images/favicon.png" />
+    <meta name="viewport" content="width=devise-width, initial-scale=0.5">
+    <title> 
+            LES KOKO BONGO ON É LAAAAA
+    </title>
+    <link rel="stylesheet" href="../../Front/style.css"/>
+    <script src="../JS/ajax.js"></script>
+    <script type="text/javascript" src="../JS/gererPage.js"></script>
+</head>
+<body>
+    <div class="contentAdmin">
+        <div class="navBar">
+            <ul>
+                <li>
+                    <h2 class="clickable" onclick="gererPage()">Gérer les pages</h2>
+                    <p>c la description fréro</p>
+                </li>
+                <li>
+                    <h2 class="clickable">Gérer les pages</h2>
+                    <p>c la description fréro</p>
+                </li>
+                <li>
+                    <h2 class="clickable">Gérer les pages</h2>
+                    <p>c la description fréro</p>
+                </li>
+                <li>
+                    <h2 class="clickable">Gérer les pages</h2>
+                    <p>c la description fréro</p>
+                </li>
+                <li>
+                    <h2 class="clickable">Gérer les pages</h2>
+                    <p>c la description fréro</p>
+                </li>
+                <li>
+                    <h2 class="clickable">Gérer les pages</h2>
+                    <p>c la description fréro</p>
+                </li>
+                <li>
+                    <h2 class="clickable">Gérer les pages</h2>
+                    <p>c la description fréro</p>
+                </li>
+            </ul>
+        </div>
+        <div class="editor" id="editor"></div>
+
+    </div>
+</body>
