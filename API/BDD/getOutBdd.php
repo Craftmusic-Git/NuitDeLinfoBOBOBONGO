6b5d92cfa301db9e7fe21a4bf2493759a446f80c
thib_info <thibault.b45@gmail.com>
2021-12-03 06:24:17 +0100

    advance

--- API/BDD/getOutBdd.php
+++ API/BDD/getOutBdd.php
@@ -11,3 +11,23 @@ function getAllArticles(){
 
     return $res;
 }
+
+function fromContenuToArticle($content){
+    $con = connect();
+
+    if(!$res = $con->query("CALL FromContenuToArticle('$content')")){
+        echo 'Error during research of article by content' . $con->error();
+    }
+
+    return $res;
+}
+
+function fromTitreToArticle($title){
+    $con = connect();
+
+    if(!$res = $con->query("CALL FromTitreToArticle('$title', 1)")){
+        echo 'Error during research of article by title' . $con->error();
+    }
+
+    return $res;
+}
